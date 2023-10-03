602f2421977bd622f7f003a6c75fbcc5494c0082
masudrana25 <mdmasudrana2512000@gmail.com>
2023-10-04 02:39:13 +0600

    Fsdfsfv

---
+++ 0. Project/7. E-Commerce Site Project 6/scripts/script.js
@@ -0,0 +1,12 @@
+const menu_bar = document.getElementById('menu-bar');
+const menu = document.getElementById('menu');
+
+menu_bar.addEventListener('click', function () {
+  if (menu.className === "navbar-hidden") {
+    menu.classList.remove('navbar-hidden');
+  } else {
+    menu.classList.add('navbar-hidden');
+  }
+
+  console.log('this.click');
+});
